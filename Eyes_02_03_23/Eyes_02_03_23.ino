7598cef6bf67d2bfa06463f1773cec156126b8e1
Max Bogaers <bogaers@qcast.nl>
2023-05-16 22:04:23 +0200

    aaaaaaaaaaa

--- Eyes_02_03_23/Eyes_02_03_23.ino
+++ Eyes_02_03_23/Eyes_02_03_23.ino
@@ -1,22 +1,9 @@
 #include <Servo.h>
 #include <Wire.h>
-#include <MFRC522.h>
-#include <SPI.h>
 
 #define SS_PIN 10
 #define RST_PIN 9
 
-#define CMD_SEL_DEV           0X09    //-> Select device.
-#define DEV_TF                0X02    //-> 02 for TF card.
-#define CMD_PLAY_FOLDER_FILE  0X0F    //-> Play with folder and file name.
-#define CMD_PLAY              0X0D    //-> Resume playback
-#define CMD_PLAY_W_INDEX      0X03
-#define CMD_PAUSE             0X0E    //-> Playback is paused
-
-#include <SoftwareSerial.h>
-
-SoftwareSerial mp3(5, 6); // RX, TX
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance.
 Servo servoUpDown;
 Servo servoLeftRight;
 Servo servoBlink;
@@ -30,23 +17,13 @@ int lidsClosed = 0;
 int lidsOpen = 45;
 int servoVal = 0;
 int mouthClosed = 0;
-int mouthOpen = 35; 
+int mouthOpen = 35;
 int switchState = 1;
 
-static int8_t Send_buf[8] = {0}; //-> Buffer to send commands. 
-static uint8_t ansbuf[10] = {0}; //-> Buffer to receive response.  
-
+bool working = false;
 void setup() {
-  pinMode(A1, INPUT);
-  Serial.begin(9600);   // Initiate a serial communication
-  mp3.begin(9600);
-  delay(500); // wait for the module to start up
-  SPI.begin();      // Initiate  SPI bus
-  mfrc522.PCD_Init();   // Initiate MFRC522
 
-  sendMp3Command(CMD_SEL_DEV, 0, DEV_TF);
-  delay(500);
-
+  Serial.begin(9600);  // Initiate a serial communication
   servoUpDown.attach(2);
   servoLeftRight.attach(3);
   servoBlink.attach(4);
@@ -55,58 +32,23 @@ void setup() {
   servoLeftRight.write(maxRight + maxLeft / 2);
   servoBlink.write(lidsOpen);
   servoMouth.write(mouthClosed);
-
-  Serial.println("[EM Controller]: Setup complete");
+  Serial.println("[EYES Controller]: Setup complete");
+  Wire.begin(8);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
 }
 
 void loop() {
-  // if (mp3.available()){
-  //   Serial.println("[EM Controller]: MP3 ANSWER: " + decodeMP3Answer());
-  // }
-
-  // Look for new cards
-  if (!mfrc522.PICC_IsNewCardPresent()) {
-    return;
-  }
-
-  // Select one of the cards
-  if (!mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
-
-  String nfcCardId = "";
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-    nfcCardId.concat(sbyte2hex(mfrc522.uid.uidByte[i]));
-
-    if (i < mfrc522.uid.size - 1) {
-      nfcCardId.concat(" ");
-    }
-  }
-
-  Serial.println("[EM Controller]: Detected nfc card: " + nfcCardId);
-
-  if(nfcCardId == "0xe3 0x51 0x66 0x2e") {
-    Serial.println("[EM Controller]: Starting blink routine...");
-
-    Blink();
-
-    sendMp3Command(CMD_PLAY_FOLDER_FILE, 0x01, 0x01);
-  }
-  else if(nfcCardId == "0x04 0x33 0x43 0x6c 0xdf 0x61 0x80") {
-    Serial.println("[EM Controller]: Starting clack routine...");
-    
+  if (working) {
     Clack();
-
-    sendMp3Command(CMD_PLAY_FOLDER_FILE, 0x01, 0x02);
-  }
-}
-
-//received data handler function
-void dataRcv(int numBytes) {
-  while(1 < Wire.available()) {
-    // loop through all but the last
-    int c = Wire.read(); // receive byte as a character
-    Serial.print(c); // print the character
+    if (random(30000) == 69) {
+      Blink();
+    }
+    if (random(30000) == 70) {
+      Look();
+    }
+    if (random(30000) == 80) {
+    }
   }
 }
 
@@ -119,170 +61,32 @@ void Blink() {
 
 void Clack() {
   for (int pos = mouthClosed; pos <= mouthOpen; pos += 1) {  // smoothly open the servo in steps of 10
-    servoMouth.write(pos);  // set the servo position
-    delay(20);           // wait for the servo to move to the new position
+    servoMouth.write(pos);                                   // set the servo position
+    delay(20);                                               // wait for the servo to move to the new position
   }
-    for (int pos = mouthOpen; pos >= mouthClosed; pos -= 10) {  // smoothly open the servo in steps of 10
-    servoMouth.write(pos);  // set the servo position
-    delay(20);           // wait for the servo to move to the new position
+  for (int pos = mouthOpen; pos >= mouthClosed; pos -= 10) {  // smoothly open the servo in steps of 10
+    servoMouth.write(pos);                                    // set the servo position
+    delay(20);                                                // wait for the servo to move to the new position
   }
 }
 
-void Look(){
-  servoLeftRight.write(maxLeft);
+void Look() {
+  for (int pos = maxRight + maxLeft / 2; pos <= maxLeft; pos -= 1) {  // smoothly open the servo in steps of 10
+    servoLeftRight.write(pos);                                        // set the servo position
+    delay(20);                                                        // wait for the servo to move to the new position
+  }
+  for (int pos = maxLeft; pos <= maxRight; pos += 1) {  // smoothly open the servo in steps of 10
+    servoMouth.write(pos);                              // set the servo position
+    delay(20);                                          // wait for the servo to move to the new position
+  }
   delay(200);
-  servoLeftRight.write(maxRight);
-  delay(200);
-  servoLeftRight.write(maxRight + maxLeft / 2);
 }
 
-void sendCommand(byte command){
-  sendMp3Command(command, 0, 0);
+void requestEvent() {
+  Wire.write("Hello ");
+  Serial.println("AAAAAAA");
 }
-
-void sendMp3Command(byte command, byte dat1, byte dat2){
-  delay(20);
-  Send_buf[0] = 0x7E;    //-> Every command should start with $(0x7E)
-  Send_buf[1] = 0xFF;    //-> Version information
-  Send_buf[2] = 0x06;    //-> The number of bytes of the command without starting byte and ending byte
-  Send_buf[3] = command; //-> Such as PLAY and PAUSE and so on
-  Send_buf[4] = 0x00;    //-> 0x00 = not feedback, 0x01 = feedback
-  Send_buf[5] = dat1;    //-> data1
-  Send_buf[6] = dat2;    //-> data2
-  Send_buf[7] = 0xEF;    //-> Ending byte of the command
-  
-  Serial.print("Sending: ");
-  
-  for (uint8_t i = 0; i < 8; i++){
-    mp3.write(Send_buf[i]);
-
-    Serial.print(sbyte2hex(Send_buf[i]));
-    if (i < 7) {
-      Serial.print(" ");
-    }
-  }
-  
-  Serial.println();
-}
-
-String sbyte2hex(uint8_t b){
-  String shex;
-
-  shex = "0x";
-
-  if (b < 16) {
-    shex += "0";
-  }
-
-  shex += String(b, HEX);
-
-  return shex;
-}
-
-// For hex to integer conversion.
-int shex2int(char *s, int n){
-  int r = 0;
-  for (int i=0; i<n; i++){
-     if(s[i]>='0' && s[i]<='9'){
-      r *= 16; 
-      r +=s[i]-'0';
-     }else if(s[i]>='A' && s[i]<='F'){
-      r *= 16;
-      r += (s[i] - 'A') + 10;
-     }
-  }
-  return r;
-}
-
-String decodeMP3Answer() {
-  String decodedMP3Answer = "";
-
-  decodedMP3Answer += sanswer();
-
-  switch (ansbuf[3]) {
-    case 0x3A:
-      decodedMP3Answer += " -> Memory card inserted.";
-      break;
-
-    case 0x3D:
-      decodedMP3Answer += " -> Completed play num " + String(ansbuf[6], DEC);
-      break;
-
-    case 0x40:
-      decodedMP3Answer += " -> Error";
-      break;
-
-    case 0x41:
-      decodedMP3Answer += " -> Data recived correctly. ";
-      break;
-
-    case 0x42:
-      decodedMP3Answer += " -> Status playing: " + String(ansbuf[6], DEC);
-      break;
-
-    case 0x48:
-      decodedMP3Answer += " -> File count: " + String(ansbuf[6], DEC);
-      break;
-
-    case 0x4C:
-      decodedMP3Answer += " -> Playing: " + String(ansbuf[6], DEC);
-      break;
-
-    case 0x4E:
-      decodedMP3Answer += " -> Folder file count: " + String(ansbuf[6], DEC);
-      break;
-
-    case 0x4F:
-      decodedMP3Answer += " -> Folder count: " + String(ansbuf[6], DEC);
-      break;
-  }
-
-  return decodedMP3Answer;
-}
-
-String sanswer(void){
-  uint8_t i = 0;
-  String mp3answer = ""; 
-
-  // Get only 10 Bytes
-  while (mp3.available() && (i < 10)){
-    uint8_t b = mp3.read();
-    ansbuf[i] = b;
-    i++;
-
-    mp3answer += sbyte2hex(b);
-  }
-
-  // if the answer format is correct.
-  if ((ansbuf[0] == 0x7E) && (ansbuf[9] == 0xEF)){
-    return mp3answer;
-  }
-
-  return "???: " + mp3answer;
-}
-
-bool StillPlaying(uint8_t TrackNumber)
-{
-  uint8_t i = 0;
-  bool state = true;
-  //String mp3answer = "";
-
-  // Get only 10 Bytes
-  while (mp3.available() && (i < 10))
-  {
-    uint8_t b = mp3.read();
-    ansbuf[i] = b;
-    i++;
-    //mp3answer += sbyte2hex(b);
-  }
-
-  //Serial.println(mp3answer);
-
-  // if the answer format is correct and file playing is complete
-  if ((ansbuf[3] == 0x3D) && (ansbuf[6] == TrackNumber))
-  {
-    state = false;
-  }
-
-  return state;
+void receiveEvent(int bytes) {
+  working = bytes == 0;
+  Serial.println("BBBB");
 }
--- Neck/Neck.ino
+++ Neck/Neck.ino
@@ -1,11 +1,12 @@
 #include <Servo.h>
+#include <Wire.h>
 
 Servo servoRotate;
 Servo servoRight;
 Servo servoLeft;
+bool working = true;
 
-void setup() 
-{
+void setup() {
   Serial.begin(9600);
   servoRotate.attach(2);
   servoLeft.attach(3);
@@ -13,49 +14,87 @@ void setup()
   servoRotate.write(90);
   servoRight.write(180);
   servoLeft.write(0);
-  
+  // Start the I2C Bus as Slave on address 9
+  Wire.begin(9);
+  // Attach a function to trigger when something is received.
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
 }
 
-void loop()
-{
-  // servoRotate.write(90-
-  if(random(30000) == 69) {
-    look();
-  }
-  else if(random(30000) == 70){
-    nod();
+void loop() {
+  if (working) {
+    if (random(3000) == 69) {
+      look();
+    } else if (random(3000) == 70) {
+      nod();
+    }
   }
 }
 
-void look(){
-    for (int pos = 90; pos <= 90+15; pos += 1) {  // smoothly open the servo in steps of 10
-    servoRotate.write(pos);  // set the servo position
-    delay(20);           // wait for the servo to move to the new position
+void look() {
+  int dir = random(0, 3);
+  Serial.println(dir);
+  int pos = 90;
+  switch (dir) {
+    case 0:
+    pos += random(5, 15);
+    Serial.println(pos);
+    // servoRotate.write(pos); 
+    delay(50);
+    break;
+
+    case 1:
+    pos -= random(5, 15);
+    Serial.println(pos);
+    // servoRotate.write(pos); 
+    delay(50);
+    break;
+
+    case 2:
+    pos += 0;
+    delay(random(50,150));
+    Serial.println(pos);
+    // servoRotate.write(pos); 
+    break;
   }
-  delay(800);
-      for (int pos = 90+15; pos <= 90-15; pos -= 1) {  // smoothly open the servo in steps of 10
-    servoRotate.write(pos);  // set the servo position
-    delay(20);           // wait for the servo to move to the new position
-  }
-  delay(800);
-  for (int pos = 90-15; pos <= 90; pos += 1) {  // smoothly open the servo in steps of 10
-    servoRotate.write(pos);  // set the servo position
-    delay(20);           // wait for the servo to move to the new position
-  }
+
+  // for (int pos = 90; pos <= 105; pos += random(1,5)) {  // smoothly open the servo in steps of 10
+  //   // servoRotate.write(pos);                       // set the servo position
+  //   delay(80);                                    // wait for the servo to move to the new position
+  //   Serial.println(pos);
+  // }
+  // for (int pos = 105; pos >= 75; pos -= random(1,5)) {  // smoothly open the servo in steps of 10
+  //   // servoRotate.write(pos);                            // set the servo position
+  //   delay(80);                                         // wait for the servo to move to the new position
+  //   Serial.println(pos);
+  // }
+  // for (int pos = 75 ; pos <= 90; pos +=  random(1,5)) {  // smoothly open the servo in steps of 10
+  // // servoRotate.write(pos);                       // set the servo position
+  //   delay(80);                                    // wait for the servo to move to the new position
+  //   Serial.println(pos);
+  // }
 }
-void nod(){
+void nod() {
   servoLeft.write(15);
-  servoRight.write(180-15);
+  servoRight.write(180 - 15);
   delay(80);
   servoLeft.write(30);
-  servoRight.write(180-30);
+  servoRight.write(180 - 30);
   delay(80);
-    servoLeft.write(45);
-  servoRight.write(180-45);
+  servoLeft.write(45);
+  servoRight.write(180 - 45);
   delay(80);
-    servoLeft.write(30);
-  servoRight.write(180-30);
+  servoLeft.write(30);
+  servoRight.write(180 - 30);
   delay(80);
   servoLeft.write(0);
   servoRight.write(180);
+}
+void requestEvent() {
+  Wire.write("Hello ");
+  Serial.println("AAAAAAA");
+}
+void receiveEvent(int bytes) {
+  working = bytes == 0;
+  Serial.println("BBBB");
 }
