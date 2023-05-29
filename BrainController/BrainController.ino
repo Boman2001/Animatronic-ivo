41b9e2c3857c2c64079d5894961a2a0bd82910db
Max Bogaers <bogaers@qcast.nl>
2023-05-29 20:29:37 +0200

    werkend

--- BrainController/BrainController.ino
+++ BrainController/BrainController.ino
@@ -2,6 +2,7 @@
 #include <MFRC522.h>
 #include <SPI.h>
 #include <MD_YX5300.h>
+#include <Adafruit_NeoPixel.h>
 
 #define SS_PIN 10
 #define RST_PIN 9
@@ -12,11 +13,16 @@
 #define CMD_PLAY 0X0D              //-> Resume playback
 #define CMD_PLAY_W_INDEX 0X03
 #define CMD_PAUSE 0X0E  //-> Playback is paused
+#define CMD_PLAY_W_VOL 0X22
+
+#define PIN 2  // Marked D1 on GEMMA
+#define NUM_LEDS 25
 
 #include <SoftwareSerial.h>
 
-SoftwareSerial mp3(5, 4);          // RX, TX
+SoftwareSerial mp3(5, 6);          // RX, TX
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance.
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
 static int8_t Send_buf[8] = { 0 };  //-> Buffer to send commands.
 static uint8_t ansbuf[10] = { 0 };  //-> Buffer to receive response.
@@ -37,19 +43,23 @@ void setup() {
   mfrc522.PCD_Init();
   Wire.begin();
 
+  strip.begin();
+  strip.setBrightness(100);  //adjust brightness here
+  strip.show();
+
   // Wait for the module to start up
   delay(500);
-  sendMp3Command(CMD_SEL_DEV, 0, DEV_TF);
+  sendCommand(CMD_SEL_DEV, DEV_TF);  //select the TF card
   delay(500);
 
   mfrc522.PCD_DumpVersionToSerial();
   Serial.println("[BrainController]: Setup complete");
-
-  sendMp3Command(CMD_PLAY_FOLDER_FILE, 0x01, 0x01);
+  sendCommand(CMD_PAUSE, 0X0E);  //pause the playing track
+    // sendCommand(CMD_PLAY_W_VOL, 0X6401);//play the first song with volume 30 class
 }
 
 void loop() {
-
+  breathe(20, 100, 0,255,25);    
   // Look for new cards
   if (!mfrc522.PICC_IsNewCardPresent()) {
     return;
@@ -72,45 +82,109 @@ void loop() {
 
   Serial.println("[BrainController]: Detected nfc card: " + nfcCardId);
 
-  if (nfcCardId == "0xe3 0x51 0x66 0x2e") {
+  if (nfcCardId == "0xe3 0x1d 0xac 0x2e") {
     Serial.println("[BrainController]: Starting blink routine...");
-    sendMp3Command(CMD_PLAY_FOLDER_FILE, 0x01, 0x01);
+    sendCommand(CMD_PLAY_W_VOL, 0X3401);
+    startTheRest();
+    delay(182000);
+    stopTheRest();
+  } else if (nfcCardId == "0x04 0x33 0x43 0x6c 0xdf 0x61 0x80") {
+    Serial.println("[BrainController]: Starting clack routine...");
+    sendCommand(CMD_PLAY_W_VOL, 0X6401);
     startTheRest();
     // delay(182000);
     stopTheRest();
-  }
-  else if (nfcCardId == "0x04 0x33 0x43 0x6c 0xdf 0x61 0x80") {
-    Serial.println("[BrainController]: Starting clack routine...");
-    sendMp3Command(CMD_PLAY_FOLDER_FILE, 0x01, 0x01);
+  } else if (nfcCardId == "0xd3 0xa7 0x2d 0x2e") {
+    Serial.println("[BrainController]: Starting blink routine...");
+    sendCommand(CMD_PLAY_W_VOL, 0X3401);
     startTheRest();
-    // delay(182000);
+    delay(182000);
+    stopTheRest();
+  } else if (nfcCardId == "0x04 0xbe 0x37 0x7f 0xdf 0x61 0x80") {
+    Serial.println("[BrainController]: Starting blink routine...");
+    sendCommand(CMD_PLAY_W_VOL, 0X3401);
+    startTheRest();
+    delay(182000);
+    stopTheRest();
+  } else if (nfcCardId == "0x04 0xa7 0x4a 0x6c 0xdf 0x61 0x80") {
+    Serial.println("[BrainController]: Starting blink routine...");
+    sendCommand(CMD_PLAY_W_VOL, 0X3401);
+    startTheRest();
+    delay(182000);
+    stopTheRest();
+  } else if (nfcCardId == "0x04 0x57 0x2e 0x6e 0xdf 0x61 0x80") {
+    Serial.println("[BrainController]: Starting blink routine...");
+    sendCommand(CMD_PLAY_W_VOL, 0X3401);
+    startTheRest();
+    delay(182000);
+    stopTheRest();
+  } else if (nfcCardId == "0x04 0xbe 0x37 0x7f 0xdf 0x61 0x8") {
+    Serial.println("[BrainController]: Starting blink routine...");
+    sendCommand(CMD_PLAY_W_VOL, 0X3401);
+    startTheRest();
+    delay(182000);
     stopTheRest();
   }
 }
-
-void sendMp3Command(byte command, byte dat1, byte dat2) {
-  delay(20);
-  Send_buf[0] = 0x7E;     //-> Every command should start with $(0x7E)
-  Send_buf[1] = 0xFF;     //-> Version information
-  Send_buf[2] = 0x06;     //-> The number of bytes of the command without starting byte and ending byte
-  Send_buf[3] = command;  //-> Such as PLAY and PAUSE and so on
-  Send_buf[4] = 0x00;     //-> 0x00 = not feedback, 0x01 = feedback
-  Send_buf[5] = dat1;     //-> data1
-  Send_buf[6] = dat2;     //-> data2
-  Send_buf[7] = 0xEF;     //-> Ending byte of the command
-
-  Serial.print("Sending: ");
-
-  for (uint8_t i = 0; i < 8; i++) {
-    mp3.write(Send_buf[i]);
-
-    Serial.print(sbyte2hex(Send_buf[i]));
-    if (i < 7) {
-      Serial.print(" ");
-    }
+void colorWipe(uint32_t c, uint8_t wait) {
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
   }
+}
+void sendMp3Command(byte command, byte dat1, byte dat2) {
+  // delay(20);
+  // Send_buf[0] = 0x7E;     //-> Every command should start with $(0x7E)
+  // Send_buf[1] = 0xFF;     //-> Version information
+  // Send_buf[2] = 0x06;     //-> The number of bytes of the command without starting byte and ending byte
+  // Send_buf[3] = command;  //-> Such as PLAY and PAUSE and so on
+  // Send_buf[4] = 0x00;     //-> 0x00 = not feedback, 0x01 = feedback
+  // Send_buf[5] = dat1;     //-> data1
+  // Send_buf[6] = dat2;     //-> data2
+  // Send_buf[7] = 0xEF;     //-> Ending byte of the command
 
-  Serial.println();
+  // Serial.print("Sending: ");
+
+  // for (uint8_t i = 0; i < 8; i++) {
+  //   mp3.write(Send_buf[i]);
+
+  //   Serial.print(sbyte2hex(Send_buf[i]));
+  //   if (i < 7) {
+  //     Serial.print(" ");
+  //   }
+  // }
+
+  // Serial.println();
+  delay(20);
+  Send_buf[0] = 0x7e;                 //starting byte
+  Send_buf[1] = 0xff;                 //version
+  Send_buf[2] = 0x06;                 //the number of bytes of the command without starting byte and ending byte
+  Send_buf[3] = command;              //
+  Send_buf[4] = 0x00;                 //0x00 = no feedback, 0x01 = feedback
+  Send_buf[5] = (int8_t)(dat1 >> 8);  //datah
+  Send_buf[6] = (int8_t)(dat1);       //datal
+  Send_buf[7] = 0xef;                 //ending byte
+  for (uint8_t i = 0; i < 8; i++)     //
+  {
+    mp3.write(Send_buf[i]);
+  }
+}
+
+void sendCommand(int8_t command, int16_t dat) {
+  delay(20);
+  Send_buf[0] = 0x7e;                //starting byte
+  Send_buf[1] = 0xff;                //version
+  Send_buf[2] = 0x06;                //the number of bytes of the command without starting byte and ending byte
+  Send_buf[3] = command;             //
+  Send_buf[4] = 0x00;                //0x00 = no feedback, 0x01 = feedback
+  Send_buf[5] = (int8_t)(dat >> 8);  //datah
+  Send_buf[6] = (int8_t)(dat);       //datal
+  Send_buf[7] = 0xef;                //ending byte
+  for (uint8_t i = 0; i < 8; i++)    //
+  {
+    mp3.write(Send_buf[i]);
+  }
 }
 
 String sbyte2hex(uint8_t b) {
@@ -236,7 +310,7 @@ void startTheRest() {
   Wire.beginTransmission(9);  // transmit to device #9
   Wire.write(1);              // sends x
   Wire.endTransmission();     // stop transmitting
-
+  colorWipe(strip.Color(255,192, 15), 100);
   Wire.beginTransmission(8);  // transmit to device #9
   Wire.write(1);              // sends x
   Wire.endTransmission();
@@ -245,5 +319,52 @@ void startTheRest() {
 void stopTheRest() {
   Wire.beginTransmission(9);  // transmit to device #9
   Wire.write(0);              // sends x
-  Wire.endTransmission();     // stop transmitting
+  Wire.endTransmission();
+  colorWipe(strip.Color(255,192, 15), 100);  // stop transmitting
 }
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j = 0; j < 256; j++) {
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    Serial.println(WheelPos);
+    return strip.Color(0, 0, WheelPos * 3);
+  } else {
+    WheelPos -= 170;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+void breathe(int pause, int steps, byte R, byte G, byte B) { 
+
+  int tmpR, tmpG, tmpB;         // Temp values
+
+  // Fade down
+  for (int s=steps; s>0; s--) {
+    tmpR = (R * s) / steps;     // Multiply first to avoid truncation errors  
+    tmpG = (G * s) / steps;
+    tmpB = (B * s) / steps;
+    
+    for (int i=0; i<NUM_LEDS; i++) {
+      strip.setPixelColor(i,tmpR,tmpG,tmpB);
+    }
+    strip.show();
+    delay(pause);
+  }    
+
+  delay(pause * 30);
+}
+
+
--- MovementController/MovementController.ino
+++ MovementController/MovementController.ino
@@ -5,22 +5,34 @@
 Servo servoHorizontal;
 Servo servoVerticalLeft;
 Servo servoVerticalRight;
+Servo servoEyeHorizontal;
+Servo servoEyeVertical;
+Servo servoEyeLids;
 
 // Positions
-int minHorizontalPos = 0;
-int maxHorizontalPos = 0;
+int minHorizontalPos = 65;
+int maxHorizontalPos = 125;
 
-int minVerticalLeftPos = 0; //UP
-int maxVerticalLeftPos = 75; //DOWN
+int minVerticalLeftPos = 0;   //UP
+int maxVerticalLeftPos = 60;  //DOWN
 
-int minVerticalRightPos = 15; //DOWN
-int maxVerticalRightPos = 90; //UP
+int minVerticalRightPos = 30;  //DOWN
+int maxVerticalRightPos = 90;  //UP
 
+int minEyeHorizontalPos = 0;
+int maxEyeHorizontalPos = 80;
+
+int minEyeVerticalPos = 20;
+int maxEyeVerticalPos = 50;
+
+int minEyeLidsPos = 0;
+int maxEyeLidsPos = 45;
 //
 bool enabled = true;
 
 
 void setup() {
+      verticalEyeScan();
 
   // Initiate a serial communication
   Serial.begin(9600);
@@ -30,6 +42,11 @@ void setup() {
   servoVerticalLeft.attach(3);
   servoVerticalRight.attach(4);
 
+  // Attach Servos
+  servoEyeVertical.attach(5);
+  servoEyeHorizontal.attach(6);
+  servoEyeLids.attach(7);
+
   // Setup BrainController connection
   Wire.begin(8);
   Wire.onReceive(receiveEvent);
@@ -37,15 +54,126 @@ void setup() {
 
   // Write default position
   defaultPosition();
-  setNeckVertPos(20);
+  setNeckVerticalPos(50);
+  defaultFacialExpression();
 
   Serial.println("[MovementController]: Setup complete");
+
+  //   setNeckHorizontalPos(0);
+  //     delay(2000);
+  // setNeckHorizontalPos(50);
+  //   delay(2000);
+  // setNeckHorizontalPos(100);
+
+  RotateNeck();
+
 }
 
 void loop() {
-  
+  performIdle();
+  delay(500);
+  delay(2000);
+  // setNeckVerticalPos(100);
 }
 
+
+// Movement Functions
+void performIdle() {
+  Serial.println("[FaceController]: Rolling the dice...");
+  int diceRoll = random(10);
+
+  switch (diceRoll) {
+
+    case 3:
+      Serial.println("[FaceController]: Starting blink routine");
+      blink();
+      break;
+
+    case 5:
+      Serial.println("[FaceController]: Starting horizontalEyeScan routine");
+      horizontalEyeScan();
+      break;
+
+    case 7:
+      Serial.println("[FaceController]: Starting verticalEyeScan routine");
+      verticalEyeScan();
+      break;
+
+    case 8:
+          Serial.println("[FaceController]: Starting horizontalNeck routine");
+      setPosSmooth(setNeckVerticalPos, 0, 100, 1000, 250);
+      setPosSmooth(setNeckVerticalPos, 100, 0, 1000, 250);
+      break;
+    
+    case 9:
+          Serial.println("[FaceController]: Starting rotateNeck routine");
+      RotateNeck();
+      break;
+  }
+}
+
+//@FIXME: Broken function
+void setNeckVerticalPos(int percentage) {
+  Serial.println(percentage);
+  Serial.println(100 - percentage);
+
+  servoVerticalLeft.write(constrain(map(percentage, 0, 100, minVerticalLeftPos, maxVerticalLeftPos), minVerticalLeftPos, maxVerticalLeftPos));
+  servoVerticalRight.write(constrain(map(100 - percentage, 0, 100, minVerticalRightPos, maxVerticalRightPos), minVerticalRightPos, maxVerticalRightPos));
+}
+void defaultFacialExpression() {
+  setEyeLidPos(80);
+  setEyeHorizontalPos(50);
+  setEyeVerticalPos(50);
+}
+
+void setPosSmooth(void (*driver)(int percentage), int startPercentage, int endPercentage, int duration, int steps) {
+  for (int i = 0; i <= steps; i++) {
+    driver(
+      (startPercentage + (endPercentage - startPercentage) * sin((float(i) / steps) * PI / 2)));
+    delay(duration / steps);
+  }
+}
+void setEyeHorizontalPos(int percentage) {
+  servoEyeHorizontal.write(constrain(map(percentage, 0, 100, minEyeHorizontalPos, maxEyeHorizontalPos), minEyeHorizontalPos, maxEyeHorizontalPos));
+}
+
+void setEyeVerticalPos(int percentage) {
+  servoEyeVertical.write(constrain(map(percentage, 0, 100, minEyeVerticalPos, maxEyeVerticalPos), minEyeVerticalPos, maxEyeVerticalPos));
+}
+
+void setEyeLidPos(int percentage) {
+  servoEyeLids.write(constrain(map(percentage, 0, 100, minEyeLidsPos, maxEyeLidsPos), minEyeLidsPos, maxEyeLidsPos));
+}
+
+void horizontalEyeScan() {
+  setPosSmooth(setEyeHorizontalPos, 50, 100, 1000, 100);
+  delay(500);
+  setPosSmooth(setEyeHorizontalPos, 100, 0, 1000, 100);
+  delay(500);
+  setPosSmooth(setEyeHorizontalPos, 0, 50, 1000, 100);
+}
+
+void verticalEyeScan() {
+  setPosSmooth(setEyeVerticalPos, 50, 100, 1000, 100);
+  delay(500);
+  setPosSmooth(setEyeVerticalPos, 100, 0, 1000, 100);
+  delay(500);
+  setPosSmooth(setEyeVerticalPos, 0, 50, 1000, 100);
+}
+
+void RotateNeck(){
+      setPosSmooth(setNeckHorizontalPos, 50, 0, 1500, 500);
+      setPosSmooth(setNeckHorizontalPos, 0, 100, 1500, 500);
+      setPosSmooth(setNeckHorizontalPos, 100, 50, 1500, 500);
+
+}
+
+void blink() {
+  setEyeLidPos(0);
+  delay(200);
+  setEyeLidPos(100);
+  delay(400);
+}
 // Brain Connection
 void requestEvent() {
   Wire.write("Hello ");
@@ -53,30 +181,16 @@ void requestEvent() {
 }
 void receiveEvent(int bytes) {
   //enabled = bytes == 0;
-  Serial.println("BBBB");
+  Serial.println("BBBB" + bytes);
 }
 
-// Movement Functions
-
-//@FIXME: Broken function
-void setNeckVerticalPos(int percentage) {
-  servoVerticalLeft.write(constrain(map(percentage, 0, 100, minVerticalLeftPos, maxVerticalLeftPos), minVerticalLeftPos, maxVerticalLeftPos));
-  servoVerticalRight.write(constrain(map(percentage, 0, 100, minVerticalRightPos, maxVerticalRightPos), minVerticalRightPos, maxVerticalRightPos));
-}
-
-void setPosSmooth(void (*driver)(int percentage), int startPercentage, int endPercentage, int duration, int steps) {
-  for (int i = 0; i <= steps; i++) {
-    driver(
-      (startPercentage + (endPercentage - startPercentage) * sin((float(i) / steps) * PI / 2))
-    );
-    delay(duration / steps);
-  }
+void setNeckHorizontalPos(int percentage) {
+  servoHorizontal.write(constrain(map(percentage, 0, 100, minHorizontalPos, maxHorizontalPos), minHorizontalPos, maxHorizontalPos));
 }
 
 
 // Routines
 void defaultPosition() {
-
   servoVerticalLeft.write(minVerticalLeftPos);
   servoVerticalRight.write(maxVerticalRightPos);
 }
